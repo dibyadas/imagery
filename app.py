c253cd5132fdd246bb3e6dce0279e98888c04d0e
Dibya Prakash Das <dibyadascool@gmail.com>
2019-02-07 02:47:42 +0530

    added timestamp

--- app.py
+++ app.py
@@ -15,30 +15,41 @@ app = Flask(__name__)
 slack_access_token = os.environ.get("slack_access_token")
 client_id = os.environ.get("client_id")
 
-def download_file(url, file_id, channel_id, comment, user_id):
-	r = requests.get(url, headers={'Authorization': 'Bearer {}'.format(slack_access_token)})
-	data = r.content
-	imgur_upload_url = "https://api.imgur.com/3/image"
-	headers = {'Authorization': 'Client-ID {}'.format(client_id), "content-type": "multipart/form-data"}
-	r = requests.post(imgur_upload_url, headers=headers, data=data)
+def download_file(url, file_id, channel_id, comment, user_id, timestamp):
 	try:
-		link = r.json()['data']['link']
-	except KeyError:
-		link = "Failed"
-	print(link)
-	print("---Deleting File---")
-	slack_file_delete = "https://slack.com/api/files.delete?token={}&file={}"
-	resp = requests.post(slack_file_delete.format(slack_access_token, file_id))
-	if not resp.json()['ok']:
-		print("Not able to delete file")
-	try:
-		hook_headers = {"Authorization": "Bearer {}".format(slack_access_token), "Content-Type": "application/json; charset=utf-8"}
-		message_data = json.dumps({"channel": channel_id, "text": "Posted by <@{}>\n{}\n{}".format(user_id, comment, link)})
-		post_url = "https://slack.com/api/chat.postMessage"
-		link_post = requests.post(post_url, headers=hook_headers, data=message_data)
-		print(link_post.json())
-	except Exception as err:
-		print("Error : "+err)
+		r = requests.get(url, headers={'Authorization': 'Bearer {}'.format(slack_access_token)})
+		data = r.content
+		imgur_upload_url = "https://api.imgur.com/3/image"
+		headers = {'Authorization': 'Client-ID {}'.format(client_id), "content-type": "multipart/form-data"}
+		r = requests.post(imgur_upload_url, headers=headers, data=data)
+		try:
+			link = r.json()['data']['link']
+		except KeyError:
+			link = "Failed"
+		print(link)
+		print("---Deleting File---")
+		slack_file_delete = "https://slack.com/api/files.delete?token={}&file={}"
+		resp = requests.post(slack_file_delete.format(slack_access_token, file_id))
+		if not resp.json()['ok']:
+			print("Not able to delete file")
+
+		# print("here")
+		# print(timestamp)
+		resp = requests.post("https://slack.com/api/chat.delete?token={}&channel={}&ts={}".format(slack_access_token, channel_id, timestamp))
+		if not resp.json()['ok']:
+			print("Not able to delete file")
+
+		# print("here")
+		try:
+			hook_headers = {"Authorization": "Bearer {}".format(slack_access_token), "Content-Type": "application/json; charset=utf-8"}
+			message_data = json.dumps({"channel": channel_id, "text": "Posted by <@{}>\n{}\n{}".format(user_id, comment, link)})
+			post_url = "https://slack.com/api/chat.postMessage"
+			link_post = requests.post(post_url, headers=hook_headers, data=message_data)
+			print(link_post.json())
+		except Exception as err:
+			print("Error : "+err)
+	except Exception as error:
+		print("Error:- ", error)
 
 
 def delete_link(user_id, channel_id, ts):
@@ -56,7 +67,7 @@ def delete_link(user_id, channel_id, ts):
 		post_url = "https://slack.com/api/chat.delete"
 		link_delete = requests.post(post_url, headers=hook_headers, data=message_data)
 
-def send_ephemeral(user_id, channel_id, file_permalink, file_id, comment):
+def send_ephemeral(user_id, channel_id, file_permalink, file_id, comment, timestamp):
 	print('-----Sending Ephemeral-----')
 	slack_ephemeral_method = "https://slack.com/api/chat.postEphemeral"
 	slack_ephemeral_json = {
@@ -67,13 +78,13 @@ def send_ephemeral(user_id, channel_id, file_permalink, file_id, comment):
 			"text": "Would you like to upload this image to imgur?",
 			"actions": [
 				{
-					"name": "response|{}|{}|{}|{}|{}".format(user_id,channel_id,file_id,file_permalink,comment),
+					"name": "response|{}|{}|{}|{}|{}|{}".format(user_id,channel_id,file_id,file_permalink,comment, timestamp),
 					"text": "Yes, save space.",
 					"type": "button",
 					"value": "yes"
 				},
 				{
-					"name": "response|{}|{}|{}|{}|{}".format(user_id,channel_id,file_id,file_permalink,comment),
+					"name": "noresponse",
 					"text": "No, this image is private.",
 					"type": "button",
 					"value": "no",
@@ -109,12 +120,13 @@ def handle():
 		print(url_data['name'] + " : " + url_data['value'] + " : " + str(eph_value))
 		if eph_value:
 			params = url_data['name'].split('|')
-			file_permalink = params[4]
-			file_id = params[3]
-			channel_id = params[2]
-			comment = params[5]
 			user_id = params[1]
-			i = pool.apply_async(download_file, [file_permalink, file_id, channel_id, comment, user_id])
+			channel_id = params[2]
+			file_id = params[3]
+			file_permalink = params[4]
+			comment = params[5]
+			timestamp = params[6]
+			i = pool.apply_async(download_file, [file_permalink, file_id, channel_id, comment, user_id, timestamp])
 		else:
 			print('---No chosen---')
 	except Exception as err:
@@ -141,6 +153,7 @@ def hello():
 			except KeyError as e:
 				print("Err: ",e)
 
+			# print("here")
 			if json_data['event'] in temp_list:
 				raise Exception('Already received. So ignoring this')
 			temp_list.append(json_data['event'])
@@ -148,28 +161,29 @@ def hello():
 			file_id = json_data['event']['file']['id']
 			file_info = requests.get("https://slack.com/api/files.info?token={}&file={}".format(slack_access_token, file_id))
 			file_data = file_info.json()
-
+			# print(json.dumps(file_data, indent=4, sort_keys=True))
 			user_id = file_data['file']['user']
 			channel_id = file_data['file']['channels'][0]
-			timestamp = file_data['shares']['public'][channel_id][0]['ts']
-
+			timestamp = file_data['file']['shares']['public'][channel_id][0]['ts']
+			# print(channel_id, timestamp, user_id)
 			conversation = requests.get("https://slack.com/api/conversations.replies?token={}&channel={}&ts={}".format(
 											slack_access_token,
 											channel_id,
 											timestamp
 										))
 			conversation_data = conversation.json()
-			
+			# print(conversation_data)
+			# print(json.dumps(conversation_data, indent=4, sort_keys=True))
 			try:
 				comment = conversation_data['messages'][0]['text']
 			except KeyError:
 				comment = ''
-			print(comment)
+			# print(comment)
 			# print(json_data)
 			if file_data['file']['size']/(1024**2) > 20:
 				raise Exception("File too large (> 20MB)")
 			file_permalink = file_data['file']['url_private_download']
-			i = pool.apply_async(send_ephemeral, [user_id, channel_id, file_permalink, file_id, comment])
+			i = pool.apply_async(send_ephemeral, [user_id, channel_id, file_permalink, file_id, comment, timestamp])
 
 		except Exception as err:
 			print("Error:- " + err)
